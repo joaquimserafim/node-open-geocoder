eb3d48406131cf8efcac85ce61358abd542f4195
Joaquim Serafim <joaquim.serafim@gmail.com>
2013-12-25 18:13:59 +0000

    add a test unit

--- test/index.js
+++ test/index.js
@@ -3,4 +3,15 @@ var test = require('tape');
 var OpenGeocoder = require('../');
 
 
+var geo = new OpenGeocoder();
 
+test('geocode', function (t) {
+  t.plan(1);
+
+  geo.geocode('135 pilkington avenue, birmingham', function (err, res) {
+    if (err) return t.notOk(false, err);
+
+    t.ok(res !== null, JSON.stringify(res));
+  });
+
+});
