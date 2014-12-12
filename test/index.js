d93ac88898fdb2cae30d752975bf20e08caa7214
quim <quim@MacBook-Pro-de-quim.local>
2014-12-12 17:53:21 +0000

    add code coverage, jshint & codestyle

--- test/index.js
+++ test/index.js
@@ -1,41 +1,43 @@
-var test = require('tape');
+var tape = require('tape');
 
 var OpenGeocoder = require('../');
 
-
 var geo = new OpenGeocoder();
 
-test('geocode', function (t) {
-  t.plan(2);
-
+tape('geocode::1', function(assert) {
   var options = {
     addressdetails: 0,
-    polygon_kml: 1
+    'polygon_kml': 1
   };
 
-  geo.geocode('135 pilkington avenue, birmingham', options, function (err, res) {
-    if (err) return t.notOk(false, err);
-    t.ok(typeof res === 'object', JSON.stringify(res));
+  geo.geocode('135 pilkington avenue, birmingham', options,
+  function(err, res) {
+    assert.deepEqual(err, null);
+    assert.ok(typeof res === 'object', JSON.stringify(res));
+    assert.end();
   });
-
-  geo.geocode('avenida da liberdade, lisboa, portugal', function (err, res) {
-    if (err) return t.notOk(false, err);
-    t.ok(typeof res === 'object', JSON.stringify(res));
-  });
-
 });
 
-test('reverse geocoding', function (t) {
-  t.plan(2);
-
-   geo.reverse(-8.945406, 38.575078, function (err, res) {
-    if (err) return t.notOk(false, err);
-    t.ok(typeof res === 'object', JSON.stringify(res));
+tape('geocode::2', function(assert) {
+  geo.geocode('avenida da liberdade, lisboa, portugal', function(err, res) {
+    assert.deepEqual(err, null);
+    assert.ok(typeof res === 'object', JSON.stringify(res));
+    assert.end();
   });
+});
 
-  geo.reverse(-8.900349, 38.576622, function (err, res) {
-    if (err) return t.notOk(false, err);
-    t.ok(typeof res === 'object', JSON.stringify(res));
+tape('reverse geocoding::1', function(assert) {
+  geo.reverse(-8.945406, 38.575078, function(err, res) {
+    assert.deepEqual(err, null);
+    assert.ok(typeof res === 'object', JSON.stringify(res));
+    assert.end();
   });
+});
 
+tape('reverse geocoding::2', function(assert) {
+  geo.reverse(-8.900349, 38.576622, function(err, res) {
+    assert.deepEqual(err, null);
+    assert.ok(typeof res === 'object', JSON.stringify(res));
+    assert.end();
+  });
 });
