771bb5779d7d2a0b6fe301f1a9bf66080950bf8c
quim <joaquim.serafim@gmail.com>
2014-12-14 23:54:25 +0000

    code coverage 100%

--- ChangeLog.md
+++
@@ -1,9 +0,0 @@
-2013.12.30, Version 1.0.0
-
-* implemet geocode (search by address)
-* implement reverse (generates an address from a latitude and longitude)
-
-
-2014.01.03, Version 1.0.1
-
-* changed "self" to "_self", and removed from the places where not needed to check the scope
--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ var tape = require('tape');
 
 var OpenGeocoder = require('../');
 
-var geo = new OpenGeocoder();
+var geo = OpenGeocoder();
 
 tape('geocode::1', function(assert) {
   var options = {
@@ -26,6 +26,13 @@ tape('geocode::2', function(assert) {
   });
 });
 
+tape('geocode forget callback should throw', function(assert) {
+  assert.plan(1);
+  assert.throws(function() {
+    geo.geocode('avenida da liberdade, lisboa, portugal');
+  }, /The callback is missing/);
+});
+
 tape('reverse geocoding::1', function(assert) {
   geo.reverse(-8.945406, 38.575078, function(err, res) {
     assert.deepEqual(err, null);
@@ -38,6 +45,47 @@ tape('reverse geocoding::2', function(assert) {
   geo.reverse(-8.900349, 38.576622, function(err, res) {
     assert.deepEqual(err, null);
     assert.ok(typeof res === 'object', JSON.stringify(res));
+    assert.end();
+  });
+});
+
+tape('reverse geocoding forget callback should throw', function(assert) {
+  assert.plan(1);
+  assert.throws(function() {
+    geo.reverse(-8.900349, 38.576622);
+  }, /The callback is missing/);
+});
+
+tape('bad coordinates', function(assert) {
+  geo.reverse(-800.900349, -380.576622, function(err) {
+    assert.deepEqual(err.message, 'Invalid coordinates!');
+    assert.end();
+  });
+});
+
+tape('send null coordinates', function(assert) {
+  geo.reverse(null, null, function(err) {
+    assert.deepEqual(err.message, 'Invalid coordinates!');
+    assert.end();
+  });
+});
+
+tape('receive a non 200 status code form the service',
+function(assert) {
+  // lets override the open URL for this test
+  geo = new OpenGeocoder('badnominatim.openstreetmap.org');
+  geo.reverse(-80.900349, 38.576622, function(err) {
+    assert.equal(err.message, 'getaddrinfo ENOTFOUND');
+    assert.end();
+  });
+});
+
+tape('the response is not a JSON', function(assert) {
+  geo = new OpenGeocoder();
+  // override the response format for the test
+  geo.format = 'xml';
+  geo.geocode('135 pilkington avenue, birmingham', function(err) {
+    assert.equal(err.message, 'Unexpected token <');
     assert.end();
   });
 });
