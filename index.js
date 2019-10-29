2eff1361e2ab358269a5f03b215a7b390bf8eecc
joaquimserafim <joaquim.serafim@gmail.com>
2019-10-29 23:50:27 +0000

    v3.0.1

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ eslint
 no-multi-spaces: ["error", {exceptions: {"VariableDeclarator": true}}]
 padded-blocks: ["error", {"classes": "always"}]
 */
-'use strict';
+'use strict'
 
-const https = require('https');
-const parse = require('json-parse-safe');
-const isValidCoordinates = require('is-valid-coordinates');
-const isObject = require('is.object');
-const qs = require('qs');
-const getPropValue = require('get-property-value');
+const https = require('https')
+const parse = require('json-parse-safe')
+const isValidCoordinates = require('is-valid-coordinates')
+const isObject = require('is.object')
+const qs = require('qs')
+const getPropValue = require('get-property-value')
 
 class Geocoder {
-  constructor(options) {
-    this.format = '&format=json';
+
+  constructor (options) {
+    this.format = '&format=json'
     this.httpOptions = {
       hostname: getPropValue(options, 'url') || 'nominatim.openstreetmap.org',
       basePath: getPropValue(options, 'basePath') || '',
@@ -23,111 +24,112 @@ class Geocoder {
       headers: {
         'User-Agent': getPropValue(options, 'userAgent') || 'node-open-geocoder'
       }
-    };
-    this.timeout = getPropValue(options, 'timeout') || 10000;
+    }
+    this.timeout = getPropValue(options, 'timeout') || 10000
   }
 
-  geocode(addr, options) {
+  geocode (addr, options) {
     const callOptions = isObject(options)
       ? `&${qs.stringify(options)}`
-      : '&addressdetails=1&polygon_geojson=1';
+      : '&addressdetails=1&polygon_geojson=1'
 
     this.httpOptions.path =
       '/search?q=' +
       encodeURI(addr.replace(/ /g, '+')) +
       callOptions +
-      this.format;
+      this.format
 
-    return this;
+    return this
   }
 
-  reverse(lon, lat) {
+  reverse (lon, lat) {
     if (!isValidCoordinates(lon, lat)) {
       softOverrideMethod(this, 'end', (cb) => {
-        cb(new Error('Invalid coordinates!'));
-      });
+        cb(new Error('Invalid coordinates!'))
+      })
     } else {
-      this.httpOptions.path = `/reverse?lon=${lon}&lat=${lat}${this.format}&addressdetails=1`;
+      this.httpOptions.path = `/reverse?lon=${lon}&lat=${lat}${this.format}&addressdetails=1`
     }
 
-    return this;
+    return this
   }
 
-  end(cb) {
-    const req = https.get(this.httpOptions, responseHandler.bind(this, cb));
+  end (cb) {
+    const req = https.get(this.httpOptions, responseHandler.bind(this, cb))
 
-    req.setTimeout(this.timeout, timeoutCb);
+    req.setTimeout(this.timeout, timeoutCb)
 
-    req.once('error', onError);
+    req.once('error', onError)
 
-    function timeoutCb() {
-      this.timedout = true;
-      this.abort();
+    function timeoutCb () {
+      this.timedout = true
+      this.abort()
     }
 
-    function onError(err) {
+    function onError (err) {
       err = this.timedout
         ? timeoutError(err.message, this.socket.timeoutMs)
-        : err;
+        : err
 
-      cb(err);
+      cb(err)
     }
   }
+
 }
 
-module.exports = function geocoderExports(url) {
-  return new Geocoder(url);
-};
+module.exports = function geocoderExports (url) {
+  return new Geocoder(url)
+}
 
 //
 // help functions
 //
 
-function responseHandler(handler, res) {
-  const statusCode = res.statusCode;
-  const contentType = res.headers['content-type'];
+function responseHandler (handler, res) {
+  const statusCode = res.statusCode
+  const contentType = res.headers['content-type']
 
-  let error;
+  let error
 
   if (statusCode !== 200) {
-    error = new Error(`Request Failed.\nStatus Code: ${statusCode}`);
+    error = new Error(`Request Failed.\nStatus Code: ${statusCode}`)
   } else if (!/^application\/json/.test(contentType)) {
     error = new Error(
       'Invalid content-type.\n' +
         `Expected application/json but received ${contentType}`
-    );
+    )
   }
 
   if (error) {
-    res.resume();
-    return handler(error);
+    res.resume()
+    return handler(error)
   }
 
-  res.setEncoding('utf8');
+  res.setEncoding('utf8')
 
-  let rawData = '';
+  let rawData = ''
 
-  res.on('data', onData).once('end', onEnd);
+  res.on('data', onData).once('end', onEnd)
 
-  function onData(chunk) {
-    rawData += chunk;
+  function onData (chunk) {
+    rawData += chunk
   }
 
-  function onEnd() {
-    const json = parse(rawData);
+  function onEnd () {
+    const json = parse(rawData)
 
-    return json.error ? handler(json.error) : handler(null, json.value);
+    return json.error ? handler(json.error) : handler(null, json.value)
   }
 }
 
-function timeoutError(reason, timeout) {
-  const err = new Error(`${reason} ${timeout}ms exceeded`);
-  err.timeout = timeout;
-  err.code = 'ECONNABORTED';
+function timeoutError (reason, timeout) {
+  const err = new Error(`${reason} ${timeout}ms exceeded`)
+  err.timeout = timeout
+  err.code = 'ECONNABORTED'
 
-  return err;
+  return err
 }
 
-function softOverrideMethod(self, methodName, newMethod) {
-  self[methodName] = newMethod;
+function softOverrideMethod (self, methodName, newMethod) {
+  self[methodName] = newMethod
 }
